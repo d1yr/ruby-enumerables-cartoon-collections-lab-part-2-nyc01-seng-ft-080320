d812aeb4feb415bbbaff61e158c17c4142bb08cb
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-29 05:07:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,10 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  planeteer_calls.map{|i|i.length>=4}
+  planeteer_calls.map{|i|
+   if i.length>=4
+    puts 
+  }
 end
 
 def find_valid_calls(planeteer_calls)
